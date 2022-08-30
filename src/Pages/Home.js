1eb305dcf6eb1345364fe46c73acf531f71ef63a
sarmulad <salammulad@gmail.com>
2022-08-30 16:19:29 +0100

    logged

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from 'antd/lib/layout/layout';
 // import MainScreen from '../components/MainScreen';
-import MainBody from '../components/MainBody';
+// import MainBody from '../components/MainBody';
 
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
       <Header />
       {/* <MainScreen /> */}
-      <MainBody />
+      {/* <MainBody /> */}
     </div>
   )
 }
