e6688e173c77216de7e7f4672bfdab900bb5edf1
sarmulad <salammulad@gmail.com>
2022-08-30 15:31:22 +0100

    log

--- src/Pages/More.js
+++ src/Pages/More.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Header } from 'antd/lib/layout/layout';
 import { Col, Row, Image } from 'antd';
-// import Bonus from '../Components/Bonus'
 import BreadCrumb from '../Components/TokeCrumb';
 import Footer from '../Components/AppFooter';
 import img from '../assets/13t.png';
@@ -16,7 +15,6 @@ const More= () => {
           <Image src={img} preview={false} />
         </Col>
       </Row>
-      {/* <Bonus /> */}
       <Footer />
     </div>
   )
